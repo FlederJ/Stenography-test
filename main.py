efbbd3be75d432fad741468a83a377675e518852
FlederJ <96187657+FlederJ@users.noreply.github.com>
2023-06-26 10:09:25 +0000

    added functions to get pixels from a png and encode those pixels with the message

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 import base64
+import png
+
+ENDOFMESSAGE = "0100100101010101010101100100111101010010010001010011100101000111010101000101010101010110010101000101010100110000010001100100100001010010010100110100010100111101"  # this will tell the decoder when the message stops, as this combination of 0 and 1 is too unlikely to happen randomly in a message
 
 
-def encode_message_as_bytestring(message):
+def enc_as_bytestr(message):
     b64 = message.encode("utf8")
     bytes_ = base64.encodebytes(b64)
     bytestring = "".join(["{:08b}".format(x) for x in bytes_])
+    bytestring += ENDOFMESSAGE
     return bytestring
+
+
+def get_px_from_img(fname):
+    img = png.Reader(fname).read()
+    pixels = img[2]
+    return pixels
+
+def encode_pixels_with_message(pixels, bytestring):
+    '''modifies pixels to encode the contents from bytestring'''
+
+    enc_pixels = []
+    string_i = 0
+    for row in pixels:
+        enc_row = []
+        for i, char in enumerate(row):
+            if string_i >= len(bytestring):
+                pixel = row[i]
+            else:
+                if row[i] % 2 != int(bytestring[string_i]):
+                    if row[i] == 0:
+                        pixel = 1
+                    else:
+                        pixel = row[i] - 1
+                else:
+                    pixel = row[i]
+            enc_row.append(pixel)
+            string_i += 1
+
+        enc_pixels.append(enc_row)
+    return enc_pixels
