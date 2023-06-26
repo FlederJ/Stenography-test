359ddf2582738eab4882dd6ea69a1c026caa50e6
FlederJ <96187657+FlederJ@users.noreply.github.com>
2023-06-26 10:38:32 +0000

    completed the other functions and added an interface

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import base64
 import png
+import base64
 
-ENDOFMESSAGE = "0100100101010101010101100100111101010010010001010011100101000111010101000101010101010110010101000101010100110000010001100100100001010010010100110100010100111101"  # this will tell the decoder when the message stops, as this combination of 0 and 1 is too unlikely to happen randomly in a message
+PROMPT = """
+Welcome to basic steganography. Please choose:
+
+1. To encode a message into an image
+2. To decode an image into a message
+q. to exit
+"""
+
+ENDOFMESSAGE = "0100100101010101010101100100111101010010010001010011100101000111010101000101010101010110010101000101010100110000010001100100100001010010010100110100010100111101"
 
 
-def enc_as_bytestr(message):
+def encode_message_as_bytestring(message):
     b64 = message.encode("utf8")
     bytes_ = base64.encodebytes(b64)
     bytestring = "".join(["{:08b}".format(x) for x in bytes_])
@@ -12,10 +20,11 @@ def enc_as_bytestr(message):
     return bytestring
 
 
-def get_px_from_img(fname):
+def get_pixels_from_image(fname):
     img = png.Reader(fname).read()
     pixels = img[2]
     return pixels
+
 
 def encode_pixels_with_message(pixels, bytestring):
     '''modifies pixels to encode the contents from bytestring'''
@@ -40,3 +49,55 @@ def encode_pixels_with_message(pixels, bytestring):
 
         enc_pixels.append(enc_row)
     return enc_pixels
+
+
+def write_pixels_to_image(pixels, fname):
+    png.from_array(pixels, 'RGB').save(fname)
+
+
+def decode_pixels(pixels):
+    bytestring = []
+    for row in pixels:
+        for c in row:
+            bytestring.append(str(c % 2))
+    bytestring = ''.join(bytestring)
+    message = decode_message_from_bytestring(bytestring)
+    return message
+
+
+def decode_message_from_bytestring(bytestring):
+    bytestring = bytestring.split(ENDOFMESSAGE)[0]
+    message = int(bytestring, 2).to_bytes(len(bytestring) // 8,
+                                          byteorder='big')
+    message = base64.decodebytes(message).decode("utf8")
+    return message
+
+
+def main():
+    print(PROMPT)
+    user_inp = ""
+    while user_inp not in ("1", "2", "q"):
+        user_inp = input("Your choice: ")
+
+    if user_inp == "1":
+        in_image = input("Please enter filename of existing PNG image: ")
+        in_message = input("Please enter the message to encode: ")
+
+        print("-ENCODING-")
+        pixels = get_pixels_from_image(in_image)
+        bytestring = encode_message_as_bytestring(in_message)
+        epixels = encode_pixels_with_message(pixels, bytestring)
+        write_pixels_to_image(epixels, in_image + "-enc.png")
+        print("[DONE] -ENCODING-")
+
+    elif user_inp == "2":
+        in_image = input(
+            "Please enter the filename of an existing PNG image: ")
+        print("-DECODING-")
+        pixels = get_pixels_from_image(in_image)
+        print(decode_pixels(pixels))
+        print("[DONE] -DECODING-")
+
+
+if __name__ == "__main__":
+    main()
